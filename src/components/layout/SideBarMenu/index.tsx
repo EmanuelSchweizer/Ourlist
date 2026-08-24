57efe98287a5a5b7988ce85f4dc8eb70136d8163
Emanuel Schweizer <emanuel.schweizer@icloud.com>
2026-08-24 22:28:38 +0200

    fix: add aria-label to sidebar open button for improved accessibility

--- src/components/layout/SideBarMenu/index.tsx
+++ src/components/layout/SideBarMenu/index.tsx
@@ -23,7 +23,7 @@ export function SideBarMenu() {
 
     return (
         <Drawer >
-            <Button intent="secondary" onPress={() => setIsOpen(true)}>
+            <Button aria-label="open sidebar" intent="secondary" onPress={() => setIsOpen(true)}>
                 <FiMenu height={20} width={20} className="text-gray-900" />
             </Button>
             <Drawer.Backdrop isOpen={isOpen} onOpenChange={setIsOpen}>
